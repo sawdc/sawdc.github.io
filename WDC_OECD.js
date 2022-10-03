bd9adb7eb9b76554a31d40ec3d11079ddf5de30d
sawdc <74550442+sawdc@users.noreply.github.com>
2022-10-03 11:24:12 +0300

    Update WDC_OECD.js

--- WDC_OECD.js
+++ WDC_OECD.js
@@ -179,7 +179,7 @@ var formObj = {
 		TableCode: $('#url').val().trim().split("/").slice(5,6)
              };
                 tableau.connectionData = JSON.stringify(formObj); // Use this variable to pass data to your getSchema and getData functions
-                tableau.connectionName = $('#url').val().trim().split("/").slice(5,6);//formObj.TableCode;// This will be the data source name in Tableau
+                tableau.connectionName = "1"+formObj.url;// This will be the data source name in Tableau
                 tableau.submit(); // This sends the connector object to Tableau
     
         });
