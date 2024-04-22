6518b97f4b751b8052ffa3fd0ed6ad4ee7525218
sawdc <74550442+sawdc@users.noreply.github.com>
2024-04-22 14:41:25 +0300

    Update stat_ee_WDC.js (lisatud substring(0,4) Vaatlusperioodi CellsNo'le

--- stat_ee_WDC.js
+++ stat_ee_WDC.js
@@ -49,9 +49,9 @@ if (MetaData_est.variables[i].code !== "Aasta" && MetaData_est.variables[i].code
 		for (var j = 0; j < MetaData_est.variables[i].values.length; j++) {
           		if (MetaData_est.variables[i].values[j].substring(0,4) == MetaData_est.variables[i].values[0].substring(0,4)) {RefPerNo=RefPerNo+1;}
         	};
-	CellsNo=CellsNo*(MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0])-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo);
+	CellsNo=CellsNo*(MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0].substring(0,4))-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo);
 	DimAasta=i; 
-	if (MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0])-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo > Dim_max_length) {Dim_max=i; Dim_max_length=MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0])-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo};
+	if (MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0].substring(0,4))-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo > Dim_max_length) {Dim_max=i; Dim_max_length=MetaData_est.variables[i].values.length - (Math.max(StartYear, MetaData_est.variables[i].values[0])-MetaData_est.variables[i].values[0].substring(0,4))*RefPerNo};
 		};
 	}
 	if (MetaData_est.variables[DimAasta].values[MetaData_est.variables[DimAasta].values.length-1].substring(0,4)<StartYear) {
